a0fa7cd3e2a1f5b408ca012363ab30ac833a393b
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-09-23 16:39:07 +0300

    [CSL-1554] Move blockgen to auxx

--- lib/src/Pos/Generator/Block/Logic.hs
+++ lib/src/Pos/Generator/Block/Logic.hs
@@ -29,8 +29,8 @@ import           Pos.Crypto                  (pskDelegatePk)
 import           Pos.DB.DB                   (getTipHeader)
 import           Pos.Delegation.Logic        (getDlgTransPsk)
 import           Pos.Generator.Block.Error   (BlockGenError (..))
-import           Pos.Generator.Block.Mode    (BlockGenMode, BlockGenRandMode,
-                                              MonadBlockGen, mkBlockGenContext,
+import           Pos.Generator.Block.Mode    (BlockGenRandMode, MonadBlockGen,
+                                              MonadBlockGenInit, mkBlockGenContext,
                                               usingPrimaryKey, withCurrentSlot)
 import           Pos.Generator.Block.Param   (BlockGenParams, HasBlockGenParams (..))
 import           Pos.Generator.Block.Payload (genPayload)
@@ -48,9 +48,9 @@ import           Pos.Util.Util               (maybeThrow, _neHead)
 
 type BlockTxpGenMode g ctx m =
     ( RandomGen g
-    , MonadBlockGen ctx m
+    , MonadBlockGenInit ctx m
     , Default (MempoolExt m)
-    , MonadTxpLocal (BlockGenMode (MempoolExt m) m)
+    , MonadTxpLocal m
     )
 
 -- | Generate an arbitrary sequence of valid blocks. The blocks are
@@ -76,7 +76,12 @@ genBlocks params = do
 -- Generate a valid 'Block' for the given epoch or slot (genesis block
 -- in the former case and main block the latter case) and apply it.
 genBlock ::
-       forall g ctx m . BlockTxpGenMode g ctx m
+       forall g ctx m.
+       ( RandomGen g
+       , MonadBlockGen ctx m
+       , Default (MempoolExt m)
+       , MonadTxpLocal m
+       )
     => EpochOrSlot
     -> BlockGenRandMode (MempoolExt m) g m (Maybe (Blund SscGodTossing))
 genBlock eos = do
